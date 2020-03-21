092d7e52a55b6764ba822ca5197c4785c13a806a
georgi.marcenkov <georgi.marcenkov@3web-studio.com>
2020-03-21 20:13:06 +0200

    documents

---
+++ src/client/desktop/modules/bishop/BishopContainer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import mitropolit from '../../../../mitropolit.png'
+
+class BishopContainer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+
+    }
+
+    render() {
+        return (
+            <div>
+                <img src={mitropolit} alt='mitropolit'/>
+vvdfdfd
+            </div>
+        );
+    }
+}
+
+BishopContainer.propTypes = {};
+
+export default BishopContainer;
