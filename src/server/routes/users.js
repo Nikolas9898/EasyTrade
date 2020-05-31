7f5616f7e821d9b7eae28e8d1d89ebab73db17de
nikola <nikola@3web-studio.com>
2020-05-31 14:19:41 +0300

    added admin

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 let User = require("../models/user/user.model");
-const auth = require('../middlewares/authenticate')
+const auth = require("../middlewares/authenticate");
 
 router.route("/").get((req, res) => {
   User.find()
@@ -10,38 +10,42 @@ router.route("/").get((req, res) => {
 
 router.route("/add").post((req, res) => {
   const username = req.body.username;
-  const password = req.body.password
-  const newUser = new User({ username,password });
+  const password = req.body.password;
+  const isAdmin = req.body.isAdmin;
 
+  const newUser = new User({ username, password, isAdmin });
   newUser
     .save()
     .then(() => res.json(newUser))
+    .catch(err => {
+      res.status(400).json("Error: " + err);
+    });
+});
+
+router.route("/:id").get((req, res) => {
+  User.findById(req.params.id)
+    .then(user => res.json(user))
     .catch(err => res.status(400).json("Error: " + err));
 });
 
-router.route('/:id').get((req, res) => {
-  User.findById(req.params.id)
-      .then(exercise => res.json(exercise))
-      .catch(err => res.status(400).json('Error: ' + err));
-});
-
-router.route('/delete/:id').delete((req, res) => {
+router.route("/delete/:id").delete((req, res) => {
   User.findByIdAndDelete(req.params.id)
-      .then(() => res.json('User deleted.'))
-      .catch(err => res.status(400).json('Error: ' + err));
+    .then(() => res.json("User deleted."))
+    .catch(err => res.status(400).json("Error: " + err));
 });
 
-router.route('/edit/:id').put((req, res) => {
+router.route("/edit/:id").put((req, res) => {
   User.findById(req.params.id)
-      .then(user => {
-        user.username = req.body.username;
-        user.password = req.body.password;
-        user.date = Date.parse(req.body.date);
+    .then(user => {
+      user.username = req.body.username;
+      user.password = req.body.password;
+      user.date = Date.parse(req.body.date);
 
-        user.save()
-            .then(() => res.json('Exercise updated!'))
-            .catch(err => res.status(400).json('Error: ' + err));
-      })
-      .catch(err => res.status(400).json('Error: ' + err));
+      user
+        .save()
+        .then(() => res.json("User updated!"))
+        .catch(err => res.status(400).json("Error: " + err));
+    })
+    .catch(err => res.status(400).json("Error: " + err));
 });
 module.exports = router;
